fb616cb730542bb4a13532eb4eaf488070fa5e2b
gabrielSlima <santos.biel5@gmail.com>
2021-01-07 15:14:55 -0300

    Add modalities - partial commit

--- mitosfc/src/app/app.component.ts
+++ mitosfc/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Modality } from './interfaces/Modality'
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,14 @@ export class AppComponent {
     console.log($element);
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
+
+  modalities: Modality[] = [{
+    name: "Futsal",
+    description: "Jogo entre duas equipes com 5 jogadores cada em uma quadra esportiva",
+    color: "red-85"
+  },{
+    name: "Futebol Society",
+    description: "Jogo entre duas equipes com 7 jogadores cada em uma quadra de grama sintética",
+    color: "green-85"
+  }]
 }
---
+++ mitosfc/src/app/interfaces/Modality.ts
@@ -0,0 +1,5 @@
+export interface Modality {
+    name: string;
+    description: string;
+    color: string;
+}
