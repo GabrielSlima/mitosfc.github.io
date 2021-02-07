7e100b2d285fd7482cf09e935eb5629d4d3d2a5a
gabrielSlima <santos.biel5@gmail.com>
2021-02-06 23:17:29 -0300

    Add responsive version

--- mitosfc/src/app/main-menu/main-menu.component.ts
+++ mitosfc/src/app/main-menu/main-menu.component.ts
@@ -48,27 +48,41 @@ export class MainMenuComponent implements OnInit {
   onWindowScroll(){
     console.log(window.pageYOffset);
     let navbar_menu = document.querySelector('#responsive-nav');
-    let navbar_toggler_button = document.querySelector("#navbar-toggler-button")
-    let logo = document.querySelector("#logo")
-    
+    let navbar_toggler_button = document.querySelector("#navbar-toggler-button");
+    let logo = document.querySelector("#logo");
+    let navbar_items = document.querySelectorAll('.nav-link')
     if((navbar_menu && logo) && ((window.pageYOffset > navbar_menu.clientHeight) || !this.isMenuCollapsed)){
       console.log('White nav...');
       navbar_menu.classList.add('bg-light');
       logo.classList.add('reduced-logo');
+      navbar_menu.classList.add('navbar-border');
       if(navbar_toggler_button){
         navbar_toggler_button.classList.remove('navbar-toggler-white');
-        navbar_toggler_button.classList.add('.navbar-bordered');
+      }
+      if(window.innerWidth >= 992){
+        navbar_items.forEach(function (nav_link){
+          console.log('Black link')
+          nav_link.classList.add('black-link');
+          nav_link.classList.remove('white-link');
+        });
       }
     }
     else if(navbar_menu && logo && window.pageYOffset == 0 ){
       console.log('Transparent nav...');
       logo.classList.remove('reduced-logo');
+      navbar_menu.classList.remove('navbar-border')
       if(this.isMenuCollapsed){
         navbar_menu.classList.remove('bg-light');
         if(navbar_toggler_button){
           navbar_toggler_button.classList.add('navbar-toggler-white')
-          navbar_toggler_button.classList.remove('.navbar-bordered');
         }
+      }
+      if(window.innerWidth >= 992){
+        navbar_items.forEach(function (nav_link){
+          console.log('White link')
+          nav_link.classList.add('white-link');
+          nav_link.classList.remove('black-link');
+        });
       }
     }
   }
